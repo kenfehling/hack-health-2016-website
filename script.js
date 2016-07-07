c89f2db625ee200baec3ede07b3dc1cfa218faf2
Ken Fehling <ken@androidideas.org>
2016-07-06 23:55:10 -0400

    Started making registration modal

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+var API_HOST = 'https://hackhealth.herokuapp.com';
+//var API_HOST = 'http://127.0.0.1:5000';
+
+function setupForm($form, path, callback) {
+    var options = {
+        url: API_HOST + path,
+        success: callback
+    };
+    //$form.on('submit', function(e) {
+    //    e.preventDefault();
+    //});
+    $form.ajaxForm(options);
+}
+
 $(function() {
-    $('nav a').smoothScroll({
+    $("nav a").smoothScroll({
         preventDefault: true,
         offset: -50  // to account for nav bar height
     });
+
+    setupForm($('.register-form'), '/register', function(response) {
+        console.log(response);
+    });
+
+    // Wake up Heroku
+    var image = new Image();
+    image.src = API_HOST + '/pixel.gif';
 });
